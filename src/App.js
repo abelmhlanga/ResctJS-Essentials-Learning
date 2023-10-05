c41da60628954eb350ebddf483bb31d6dc7e664f
Abel Mhlanga <abelmhla@gmail.com>
2023-10-05 15:47:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>My Website</h1>
+    </div>
+  )
+}
+
+export function About() {
+  return (
+    
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>About us</h1>
+    </div>
+  )
+}
+
+export function Contact() {
+  return (
+    <div>
+            <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>Contact us</h1>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+  <h1><Home /></h1>
+  );
+}
+
